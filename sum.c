a9f136b7d01d7616c70da4a537f2d76690a7aa37
harikrishnancs99 <harikrishnancs13@gmail.com>
2021-11-25 18:02:36 +0530

    DS1

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{ 
+   int a,b,sum;
+   printf("Enter first number:");
+   scanf("%d",&a);
+   printf("Enter second number:");
+   scanf("%d",&b);
+   sum=a+b;
+   printf("The sum of two numbers:%d",sum);
+   return(0);
+}
